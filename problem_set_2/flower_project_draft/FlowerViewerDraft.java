17415e1d22d1c82232146a664a4d850edd0042cc
wittelw <wittelw@hotmail.com>
2013-12-15 19:18:49 -0800

    Add one more flower (in wrong place)

--- problem_set_2/flower_project_draft/FlowerViewerDraft.java
+++ problem_set_2/flower_project_draft/FlowerViewerDraft.java
@@ -1,11 +1,10 @@
-
-public class FlowerViewerDraft
-{
-   public static void main(String[] args)
-   {
-       Flower rose = new Flower(30, 30);
-       rose.draw();
-       rose = new Flower(80, 80);
-       rose.draw();
-   }
+public class FlowerViewerDraft {
+	public static void main(String[] args) {
+		Flower rose = new Flower(30, 30);
+		rose.draw();
+		rose = new Flower(80, 80);
+		rose.draw();
+		rose = new Flower(110, 80);
+		rose.draw();
+	}
 }
