8e379589b9586040814705bd72c234b873916c2b
wittelw <wittelw@hotmail.com>
2013-12-30 18:28:39 -0800

    edited line 7

--- problem_set_2/flower_project_draft/FlowerViewerDraft.java
+++ problem_set_2/flower_project_draft/FlowerViewerDraft.java
@@ -4,7 +4,7 @@ public class FlowerViewerDraft {
 		rose.draw();
 		rose = new Flower(80, 80);
 		rose.draw();
-		rose = new Flower(51, 60);
+		rose = new Flower(52, 60);
 		rose.draw();
 		rose = new Flower(555, 666);
 		rose.draw();
