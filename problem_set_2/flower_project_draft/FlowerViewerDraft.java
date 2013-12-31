0be3721fe54441b0177844a544e4fd5843e23d96
nigel <nigel@old-samsung.hsd1.wa.comcast.net>
2013-12-30 18:34:10 -0800

    1234567890

--- problem_set_2/flower_project_draft/FlowerViewerDraft.java
+++ problem_set_2/flower_project_draft/FlowerViewerDraft.java
@@ -4,11 +4,7 @@ public class FlowerViewerDraft {
 		rose.draw();
 		rose = new Flower(80, 80);
 		rose.draw();
-<<<<<<< HEAD
-		rose = new Flower(75, 75);
-=======
-		rose = new Flower(52, 60);
->>>>>>> branch 'master' of https://github.com/wittelw/cs046-problem_set_2
+		rose = new Flower(65, 65);
 		rose.draw();
 		rose = new Flower(555, 666);
 		rose.draw();
